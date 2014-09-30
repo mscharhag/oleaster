1bce160c11d1ffc616186ce918305c46b52d52b0
Michael Scharhag <m.scharhag@gmail.com>
2014-09-30 12:43:58 +0200

    Added javadoc to Matchers

--- oleaster-matcher/src/main/java/com/mscharhag/oleaster/matcher/Matchers.java
+++ oleaster-matcher/src/main/java/com/mscharhag/oleaster/matcher/Matchers.java
@@ -17,44 +17,184 @@ package com.mscharhag.oleaster.matcher;
 
 import com.mscharhag.oleaster.matcher.matchers.*;
 
+/**
+ * {@code Matchers} provides a set of static {@code expect()} methods that can (and should) be used to
+ * obtain Matcher instances.
+ * <p>The returned Matcher instances can be used to check test results.
+ * <p>For example
+ * <pre>
+ *     expect(40 + 2).toEqual(42);
+ *     expect(person).toBeNotNull();
+ *     expect("foo").toStartWith("fo");
+ * </pre>
+ * <p>For fluent access, it is recommended to statically import the methods of this class.
+ */
 public class Matchers {
 
+	private Matchers() {
+		// no instance needed
+	}
+
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code long} values.
+	 * <pre>
+	 *     expect(42l).toEqual(42);
+	 *     expect(42l).toBeGreaterThan(41);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code IntegerNumberMatcher} initialized with {@code value}
+	 */
 	public static IntegerNumberMatcher expect(long value) {
 		return new IntegerNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code int} values.
+	 * <pre>
+	 *     expect(42).toEqual(42);
+	 *     expect(42).toBeGreaterThan(41);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code IntegerNumberMatcher} initialized with {@code value}
+	 */
 	public static IntegerNumberMatcher expect(int value) {
 		return new IntegerNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code short} values.
+	 * <pre>
+	 *     expect((short) 42).toEqual(42);
+	 *     expect((short) 42).toBeGreaterThan(41);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code IntegerNumberMatcher} initialized with {@code value}
+	 */
 	public static IntegerNumberMatcher expect(short value) {
 		return new IntegerNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code byte} values.
+	 * <pre>
+	 *     expect((byte) 42).toEqual(42);
+	 *     expect((byte) 42).toBeGreaterThan(41);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.IntegerNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code IntegerNumberMatcher} initialized with {@code value}
+	 */
 	public static IntegerNumberMatcher expect(byte value) {
 		return new IntegerNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.FloatingPointNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code double} values.
+	 * <pre>
+	 *     expect(42.0).toEqual(42.0);
+	 *     expect(41.999 + 0.001).toBeCloseTo(42);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.FloatingPointNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code FloatingPointNumberMatcher} initialized with {@code value}
+	 */
 	public static FloatingPointNumberMatcher expect(double value) {
 		return new FloatingPointNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.FloatingPointNumberMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code float} values.
+	 * <pre>
+	 *     expect(42.0f).toEqual(42.0);
+	 *     expect(41.999f + 0.001f).toBeCloseTo(42);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.FloatingPointNumberMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code FloatingPointNumberMatcher} initialized with {@code value}
+	 */
 	public static FloatingPointNumberMatcher expect(float value) {
 		return new FloatingPointNumberMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.ExceptionMatcher} initialized with
+	 * the passed {@code CodeBlock}.
+	 * <p>The returned matcher can be used to check if the passed {@code CodeBlock} throw an exception.
+	 * {@link com.mscharhag.oleaster.matcher.matchers.ExceptionMatcher.CodeBlock} is a functional interface that can be
+	 * implemented using a Java 8 Lambda expression.
+	 * <p>Example:
+	 * <pre>
+	 *     expect(() -&gt; {
+	 *         // code that should throw an IllegalArgumentException
+	 *     }).toThrow(IllegalArgumentException.class);
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.ExceptionMatcher
+	 * @param block the code block that should throw an exception
+	 * @return a new {@code ExceptionMatcher} initialized with {@code block}
+	 */
 	public static ExceptionMatcher expect(ExceptionMatcher.CodeBlock block) {
 		return new ExceptionMatcher(block);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.BooleanMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code boolean} values.
+	 * <pre>
+	 *     expect(true).toEqual(true);
+	 *     expect(true).toBeTrue();
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.BooleanMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code BooleanMatcher} initialized with {@code value}
+	 */
 	public static BooleanMatcher expect(boolean value) {
 		return new BooleanMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.StringMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code String} values.
+	 * <pre>
+	 *     expect("foo").toEqual("foo");
+	 *     expect("foo").toStartWith("fo");
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.StringMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code StringMatcher} initialized with {@code value}
+	 */
 	public static StringMatcher expect(String value) {
 		return new StringMatcher(value);
 	}
 
+	/**
+	 * Creates a new {@link com.mscharhag.oleaster.matcher.matchers.ObjectMatcher} initialized with
+	 * the passed {@code value}.
+	 * <p>The returned matcher can be used to check {@code Object} values.
+	 * <pre>
+	 *     expect(person).toEqual(new Person("john"));
+	 *     expect(person).toBeNotNull();
+	 * </pre>
+	 * @see com.mscharhag.oleaster.matcher.matchers.ObjectMatcher
+	 * @param value the value that should be checked
+	 * @return a new {@code ObjectMatcher} initialized with {@code value}
+	 */
 	public static ObjectMatcher<Object> expect(Object value) {
 		return new ObjectMatcher<>(value);
 	}
