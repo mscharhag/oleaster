6c1d1745b097af02c5fda32e711a53ca137fcdba
Peter Klijn <pklijn@xebia.com>
2017-03-24 13:07:52 +0100

    Added brackets for readability

--- oleaster-runner/src/test/java/com/mscharhag/oleaster/runner/PendingSpecTest.java
+++ oleaster-runner/src/test/java/com/mscharhag/oleaster/runner/PendingSpecTest.java
@@ -9,37 +9,47 @@ import static junit.framework.TestCase.*;
 @RunWith(OleasterRunner.class)
 public class PendingSpecTest {{
   describe("PendingSpecs", () -> {
-    describe("", () -> {
-      it("should be executed", () ->
-        assertTrue(true));
+    describe("with a nested describe", () -> {
+      it("should be executed", () -> {
+        assertTrue(true);
+      });
 
-      xit("should not be executed", () ->
-        fail("Pending it (xit) should not be executed!"));
+      xit("should not be executed", () -> {
+        fail("Pending it (xit) should not be executed!");
+      });
 
-      describe("a nested describe should be executed", () ->
-        it("should be fine", () ->
-          assertTrue(true)));
+      describe("a nested describe should be executed", () -> {
+        it("should be fine", () -> {
+          assertTrue(true);
+        });
+      });
 
       xdescribe("a pending describe should not be executed", () -> {
-        it("should not be executed", () ->
-          fail("'it' in 'xdescribe' should not be executed!"));
+        it("should not be executed", () -> {
+          fail("'it' in 'xdescribe' should not be executed!");
+        });
 
         describe("a describe with a xdescribe parent", () -> {
-          it("should not be executed", () ->
-            fail("'it' in 'describe' with 'xdescribe' parent should not be executed!"));
+          it("should not be executed", () -> {
+            fail("'it' in 'describe' with 'xdescribe' parent should not be executed!");
+          });
 
-          describe("a describe with a xdescribe grandparent", () ->
-            it("should not be executed", () ->
-              fail("'it' in 'describe' with 'xdescribe' grandparent should not be executed!")));
+          describe("a describe with a xdescribe grandparent", () -> {
+            it("should not be executed", () -> {
+              fail("'it' in 'describe' with 'xdescribe' grandparent should not be executed!");
+            });
+          });
         });
       });
     });
 
-    it("should be executed", () ->
-      assertTrue(true));
+    it("should be executed", () -> {
+      assertTrue(true);
+    });
 
-    xit("should not be executed", () ->
-      fail("Pending it (xit) should not be executed!"));
+    xit("should not be executed", () -> {
+      fail("Pending it (xit) should not be executed!");
+    });
 
     it("another pending style of test");
   });
